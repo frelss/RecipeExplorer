df492bf150d3602deb8a6edd35d7767977570f0b
frelss <originsupra@gmail.com>
2023-08-26 11:49:14 +0200

    Created config and helpers modules

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,14 +4,6 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import recipeView from './views/recipeView.js';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
